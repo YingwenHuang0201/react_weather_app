7c1e33bfaddcc4166352e1eed3eb2721013d2fca
YingwenHuang0201 <103048855+YingwenHuang0201@users.noreply.github.com>
2022-11-27 02:30:01 +0900

    added search

--- src/Weather.js
+++ src/Weather.js
@@ -5,6 +5,7 @@ import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeatherData({
@@ -19,16 +20,33 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "efb258538681079ba3b62972e9a1386b";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
                 type="search"
                 placeholder="Enter a city"
                 className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -40,9 +58,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "efb258538681079ba3b62972e9a1386b";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading...";
   }
 }
