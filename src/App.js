8a49577f036e9eb8d880dfb7a18ed45d2e7527f0
YingwenHuang0201 <103048855+YingwenHuang0201@users.noreply.github.com>
2022-11-26 15:54:38 +0900

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          by Yingwen Huang{" "}
+          <a
+            href="https://github.com/YingwenHuang0201/react_weather_app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
